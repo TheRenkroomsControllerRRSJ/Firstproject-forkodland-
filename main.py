b2867eb4fb680a3387c254d54132a1a0b2f4137d
TheRenkroomsControllerRRSJ <hello.brawl.stars.and.roblox@gmail.com>
2025-04-18 21:25:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-y = 67
-x = 93
-print(x-y)
+import time
+meme_dict = {
+        "CRINGE": "Garip ya da utandırıcı bir şey",
+        "LOL": "Komik bir şeye verilen cevap",
+        "ROFL": "bir şakaya karşılık cevap",
+        "SHEESH": "onaylamamak",
+        "CREEPY": "korkunç",
+        "AGGRO": "agresifleşmek/sinirlenmek",
+        ":D": "gülen yüz",
+        ":)": "mutlu",
+        ":(": "üzgün",
+        ";(": "ağlıyor/ağlamak"
+            }
+print("EMOJI VE MODERN SÖZLÜĞE HOŞGELDİNİZ!")
+time.sleep(1)
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!)(emojileri doğru şekilde yazınız): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("böyle bir kelime yok")
